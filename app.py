3b9ce73790615fea7f4d7274b7eb76d48e69ec89
Jonathan Ferreras <jonathanferreras@icloud.com>
2017-06-08 17:58:51 -0400

    added functionality to table 2 and table 3

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from data import extractCsv
 import csv
 app = Flask(__name__)
 
@@ -12,32 +13,39 @@ def table1():
 				  'categoryB': ['bb', 'ba', 'bc'],
 				  'categoryC': ['cc', 'ca', 'cb']}
 	category_titles = categories.keys()
-
-	reader = csv.DictReader(open('static/Table1.csv', 'rb'))					#open csv file
-	data = []																	#intialize empty array named 'data'
-	for line in reader:                        									#creates array of dictionaries
-		data.append(line)														#append each dictionary from csv into 'data'
-	fields = data[0].keys() 													#creates array of keys from 'data'
-	return render_template('table1.html', data = data, fields = fields, category_titles = category_titles, categories = categories)
-
+	data, fields = extractCsv('Table1.csv')
+	return render_template('table1.html',
+							data = data,
+							fields = fields,
+							category_titles = category_titles,
+							categories = categories)
 @app.route('/table2')
 def table2():
-    with open('static/Table2.csv', 'r') as inFile:
-        read = csv.reader(inFile)
-        data = [row for row in read]
-        fields = data[0]
-        del data[0]
-    return render_template('table2.html', data = data, fields = fields)
+	categories = {'categoryA': ['aa', 'ab', 'ac'],
+				  'categoryB': ['bb', 'ba', 'bc'],
+				  'categoryC': ['cc', 'ca', 'cb']}
+	category_titles = categories.keys()
+	data, fields = extractCsv('Table2.csv')
+
+	return render_template('table1.html',
+							data = data,
+							fields = fields,
+							category_titles = category_titles,
+							categories = categories)
 
 @app.route('/table3')
 def table3():
-    with open('static/Table3.csv', 'r') as inFile:
-        read = csv.reader(inFile)
-        data = [row for row in read]
-        fields = data[0]
-        del data[0]
-    return render_template('table3.html', data = data, fields = fields)
+	categories = {'categoryA': ['aa', 'ab', 'ac'],
+				  'categoryB': ['bb', 'ba', 'bc'],
+				  'categoryC': ['cc', 'ca', 'cb']}
+	category_titles = categories.keys()
+	data, fields = extractCsv('Table3.csv')
 
+	return render_template('table1.html',
+							data = data,
+							fields = fields,
+							category_titles = category_titles,
+							categories = categories)
 if __name__ == "__main__":
     TEMPLATES_AUTO_RELOAD = True
     app.run(debug=True)
--- data.py
+++ data.py
@@ -1,15 +1,9 @@
 import csv
+def extractCsv(csv_name):
+    reader = csv.DictReader(open('static/'+ csv_name, 'rb'))					#open csv file
+    data = []																	#intialize empty array named 'data'
+    for line in reader:                        									#creates array of dictionaries
+    	data.append(line)														#append each dictionary from csv into 'data'
+    fields = data[0].keys() 													#creates array of keys from 'data'
 
-categories = {'categoryA': ['aa', 'ab', 'ac'], 'categoryB': ['bb', 'ba', 'bc'], 'categoryC': ['cc', 'ca', 'cb']}
-category = categories.keys()
-for key in category:
-    print(categories[key])
-# reader = csv.DictReader(open('static/Table1.csv', 'rb'))
-# data = []
-# for line in reader:                             #creates array of dictionaries
-# 	data.append(line)
-# fields = data[0].keys()                    #creates array of keys from data
-#
-# for group in data:
-#     for field in fields:
-#         print(group[field])
+    return data, fields
